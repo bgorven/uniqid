31508e3a37819b85ec772ccb0f2e6935172bf4e2
Buddy <buddygorven@msn.com>
2019-01-31 13:51:31 +1300

    Change const declarations to var

--- index.js
+++ index.js
@@ -16,8 +16,8 @@ var address = '';
 if(typeof __webpack_require__ !== 'function'){
     var mac = '', networkInterfaces = require('os').networkInterfaces();
     for(interface_key in networkInterfaces){
-        const networkInterface = networkInterfaces[interface_key];
-        const length = networkInterface.length;
+        var networkInterface = networkInterfaces[interface_key];
+        var length = networkInterface.length;
         for(var i = 0; i < length; i++){
             if(networkInterface[i].mac && networkInterface[i].mac != '00:00:00:00:00:00'){
                 mac = networkInterface[i].mac; break;
